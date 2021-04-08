c7aa8db9c8b3cc5404ae6e88438f38160e8f3e67
Tom Fream <tomfream@gmail.com>
2021-04-08 15:04:38 -0600

    snake send message

--- client.js
+++ client.js
@@ -1,15 +1,15 @@
 /**
  * Establishes connection with the game server
  */
- const net = require('net');
+const net = require('net');
 
- const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function() {
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
   conn.on('connect', function() {
     console.log("Successfully connected to the game server!");
@@ -20,7 +20,7 @@
     // }, 100);
   });
 
-  conn.on('data', function (data) {
+  conn.on('data', function(data) {
     console.log(`Message from server: ${data}`);
   });
 
--- input.js
+++ input.js
@@ -1,21 +1,55 @@
-const setupInput = function() {
+let connection;
+
+// const writeMessage = require('./message');
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   
-  handleUserInput(stdin);
+  handleUserInput(stdin, conn);
   
   return stdin;
 };
 
-const handleUserInput = (stdin) => {
+const handleUserInput = (stdin, conn) => {
   stdin.on('data', (key) => {
-  // \u0003 maps to ctrl+c input
-  if (key === '\u0003') {
-    process.exit();
+    if (key === '\u0003') {
+      process.exit();
     }
-  }); 
+    if (key === 'w') {
+      conn.write('Move: up');
+    }
+    if (key === 'a') {
+      conn.write('Move: left');
+    }
+    if (key === 's') {
+      conn.write('Move: down');
+    }
+    if (key === 'd') {
+      conn.write('Move: right');
+    }
+    if (key === '/') {
+      conn.write('Say: Big snake!');
+    }
+  });
 };
 
+
+
 module.exports = { setupInput };
+
+// switch (key) {
+//   case (key === '\u0003'):
+//     process.exit();
+//   case (key === 'w'):
+//     conn.write('Move: up');
+//   case (key === 'a'):
+//     conn.write('Move: left');
+//   case (key === 's'):
+//     conn.write('Move: down');
+//   case (key === 'd'):
+//     conn.write('Move: right');
+// };
